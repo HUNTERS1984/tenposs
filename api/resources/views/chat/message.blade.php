19aa3217322c0941e0290dd29aa4b5f1b977ba2b
phanvannhien <phanvannhien@gmail.com>
2016-09-01 07:49:37 +0000

    Modifined chat view

--- api/resources/views/chat/message.blade.php
+++ api/resources/views/chat/message.blade.php
@@ -1,48 +1,206 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
-    <title>Chat</title>
-    <link rel="stylesheet" href="{{ url('assets/plugins/linechat/chat.css') }}">
-</head>
-<body>
-    <div class="chat-page">
-        <div class="chat_window">
-            <div class="top_menu">
-                <div class="buttons">
-                    <div class="button close"></div>
-                    <div class="button minimize"></div>
-                    <div class="button maximize"></div>
-                </div>
-                <div class="title">Chat</div>
-            </div>
-            <ul class="messages"></ul>
-            <div class="bottom_wrapper clearfix">
-                <div class="message_input_wrapper"><input class="message_input" placeholder="Type your message here..."/></div>
-                <div class="send_message">
-                    <div class="icon"></div>
-                    <div class="text">Send</div>
-                </div>
-            </div>
+    <title>Enduser Chat</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/front-chat.css') }} "/>
+    <script type="text/javascript" src="{{ secure_asset('assets/js/jquery-1.11.2.min.js') }} "></script>
+    <script type="text/javascript" src="{{ secure_asset('assets/plugins/jquery.scrollbar/jquery.scrollbar.min.js') }} "></script>
+  </head>
+  <body>
+    <div class="container">
+      <p>&nbsp;</p>  
+      <div class="panel panel-info">
+        <div class="panel-heading">Tenposs</div>
+        <div class="panel-body">
+          <div id="" class="rooms">
+              <ul class="messages scrollbar-macosx"></ul>
+          </div>
         </div>
-        <div class="message_template">
-            <li class="message">
-                <div class="avatar"></div>
-                <div class="text_wrapper">
-                    <div class="text"></div>
-                </div>
-            </li>
+        <div class="panel-footer">
+          <div class="input-group">
+              <input type="text" class="form-control message_input" placeholder="Enter message...">
+              <span class="input-group-btn">
+                  <button class="btn btn-default send_message" type="button">Send</button>
+              </span>
+          </div><!-- /input-group -->
         </div>
-    </div>
+      </div><!--end panel --> 
+      <div class="message_template" style="display:none">
+          <li class="message">
+              <div class="avatar">
+                <img src=""/>
+              </div>
+              <div class="text_wrapper">
+                  <div class="text"></div>
+                  <div class="timestamp"></div>
+              </div>
+          </li>
+      </div>
+      
+      <div class="message_template_system" style="display:none">
+          <li class="text-center">
+              <p class="text-muted"></p>
+          </li>
+      </div>
+      
+      
+    </div><!--end container -->
+    
 
-    <script type="text/javascript" src="{{ url('assets/js/jquery-1.11.2.min.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/plugins/linechat/chat.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script>    
+<script type="text/javascript">
+
+
+var socket;
+
+
+var profile = $.parseJSON('<?php echo ($profile) ?>');
+var room_id = '{{ $room_id }}';
+
+function drawMessage(side, profile, message){
+     
+    var $message;
+    var $messages = $('ul.messages');
+    var d = new Date();
+    $message = $($('.message_template').clone().html());
+    $message.addClass(side).find('.text').html(message);
+    $message.find('.avatar img').attr('src',profile.pictureUrl+'/small')
+    $message.find('.timestamp').text(d.getTime()/1000);
+    $messages.append($message);
+    setTimeout(function () {
+        return $message.addClass('appeared');
+    }, 0);
+    return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+}
+
+function drawSystemMessage(text){
+  $message = $($('.message_template_system').clone().html());
+  $message.find('p').text(text);
+  $('.messages').append($message);
+    return setTimeout(function () {
+        return $message.addClass('appeared');
+    }, 0);
+}
+ 
+function getMessageText() {
+   return $('.message_input').val();
+};
+
+
+function sendMessage(text) {
+    var $messages, message;
+    if (text.trim() === '') {
+        return;
+    }
+    $('.message_input').val('');
+    $messages = $('ul.messages');
+    drawMessage('right',profile, text);
+    // Send message to server
+    var d = new Date();
+    var params = {
+        'action': 'message',
+        'message': text,
+        'timestamp': d.getTime()/1000
+    };
+    conn.send(JSON.stringify(params));
+    console.log('Send message');
+    console.log(JSON.stringify(params));
+    
+    return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+};
+
+// Connect to server 
+function connectToChat() {
+    socket = new io.connect('https://tenposs-phanvannhien.c9users.io:8989', {
+        'reconnection': true,
+        'reconnectionDelay': 1000,
+        'reconnectionDelayMax' : 5000,
+        'reconnectionAttempts': 5
+    });
+    // when user connect, store the user id and name
+    socket.on('connect', function (user) {
+      console.log('request connect');
+        socket.emit('join', {id: "123", name: "Nhienphan"});
+    });
+        
+            
+    /*
+    conn.onopen = function() {
+        var params = {
+            'action': 'connect',
+            'roomId': room_id,
+            'from' : {
+              'user_type' : 'endusers',
+              'profile' : profile
+            }
+        };
+        console.log('Send request connect');
+        console.log(params);
+        console.log('----------------------------------------------------------------------------------------------');
+        conn.send(JSON.stringify(params));
+    };
+
+    // client handle message from server
+    conn.onmessage = function(e) {
+        console.log('Client handle message from server');
+        console.log(e.data);
+        console.log('---------------------------------------------------------');
+        var data = JSON.parse(e.data);
+        
+        if (data.hasOwnProperty('type')) {
+          if( data.type == 'user-connected' && data.hasOwnProperty('message_type') && data.hasOwnProperty('message')){
+            if( data.message_type == 'system_status' ){
+              drawSystemMessage(data.message);
+            }
+          }
+          
+          if (data.type = 'message' && data.hasOwnProperty('message') && data.hasOwnProperty('from')) {
+            drawMessage('left',data.from, data.message);
+          }
+          
+          if (data.type = 'user-disconnected' && data.hasOwnProperty('message_type') && data.hasOwnProperty('message')) {
+            if( data.message_type == 'system_status' ){
+              drawSystemMessage(data.message);
+            }
+          }
+        }
+        
+      
+    };
+
+    conn.onerror = function(e) {
+        console.log(e);
+    };
+    conn.onclose =function(e){
+        console.log('Connection closed');
+    };
+    */
+    return false;
+}
+
+
+
+
+
+
+$(document).ready(function(){
+    jQuery('.scrollbar-macosx').scrollbar();
+    connectToChat();
+    $('.send_message').click(function (e) {
+        return sendMessage(getMessageText());
+    });
+    $('.message_input').keyup(function (e) {
+        if (e.which === 13) {
+            return sendMessage(getMessageText());
+        }
+    });
+    
+});
+
+      
+</script>
 </body>
 </html>
-
-
-
-
-
