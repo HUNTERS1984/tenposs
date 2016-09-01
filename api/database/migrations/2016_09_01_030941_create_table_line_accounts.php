790212804a18502c9419580e797af823fc8425b1
phanvannhien <phanvannhien@gmail.com>
2016-09-01 07:48:37 +0000

    Created migration lineaccounts and seeder data appline demo

---
+++ api/database/migrations/2016_09_01_030941_create_table_line_accounts.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableLineAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('line_accounts', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->integer('app_user_id');
+            $table->string('mid')->unique();// Line ID
+            $table->string('displayName')->nullable();
+            $table->string('pictureUrl')->nullable();
+            $table->string('statusMessage')->nullable();
+            $table->string('access_token')->unique();
+            $table->string('token_type');
+            $table->string('expires_in');
+            $table->string('refresh_token');
+            $table->string('scope')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('line_accounts');
+    }
+}
