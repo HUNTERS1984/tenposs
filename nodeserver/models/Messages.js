5d09728740c7685be4c416709ba964535ff1440a
phanvannhien <phanvannhien@gmail.com>
2016-09-05 15:53:48 +0000

    chat bot to enduser

--- nodeserver/models/Messages.js
+++ nodeserver/models/Messages.js
@@ -8,26 +8,23 @@ var connection = mysql.createConnection({
 
 
 
-var Messages = {
-    getMessageHistory : function(from,to,limit,callback){
-        
-        connection.connect(function(err){
-            if(err) return;
-            var sql =   " SELECT m.room_id,m.to_mid, m.from_mid, m.message"+
-                    	" FROM messages m"+
-                    	" WHERE"+ 
-                    	" 	(m.from_mid = ? AND m.to_mid = ?)"+
-                    	"	OR (m.to_mid = ?  AND m.from_mid = ?)"+
-                    	" ORDER BY created_at DESC"+
-                    	" LIMIT ?";
-            connection.query({
-                sql: 'SELECT * FROM `books` WHERE `author` = ?',
-                    values: [from,to,from,to,limit]
-                }, function (error, results, fields) {
-                    callback(results);
-                });
-        });
-    }
-}
 
-exports = exports.module = Messages; 
+exports.getMessageHistory = function(from, to, limit, _callback){
+    
+        var sql =   " SELECT m.room_id,m.to_mid, m.from_mid, m.message"+
+                	" FROM messages m"+
+                	" WHERE"+ 
+                	" 	(m.from_mid = ? AND m.to_mid = ?)"+
+                	"	OR (m.to_mid = ?  AND m.from_mid = ?)"+
+                	" ORDER BY created_at DESC"+
+                	" LIMIT ?";
+                	
+        connection.query({
+            sql: sql,
+                values: [from,to,from,to,limit]
+            }, function (error, results, fields) {
+                if(error) return false;
+                return _callback(results[0]);
+            });
+        
+    }
--- nodeserver/server.js
+++ nodeserver/server.js
@@ -3,12 +3,15 @@ var app     = require('express')(),
     http    = require('http').Server(app),
     io      = require('socket.io')(http),
     Redis   = require('ioredis'),
-    redis   = new Redis(8082);// Start redis on port 8082
-
+    redis   = new Redis(8082),// Start redis on port 8082
+    request = require('request');
 // Models
+const API_SEND_MESSAGE = 'https://trialbot-api.line.me/v1/events';
+
+
 var Messages = require("./models/Messages");
-
-
+var Apps     = require("./models/Apps"); 
+var Bot      = require("./line/Bot"); 
 var port = 8081; // Start socket io port 8081
    
 http.listen(port, function() {
@@ -16,6 +19,8 @@ http.listen(port, function() {
 });
 
 io.on('connection', function (socket_client) {
+    
+    
 
     socket_client.on('join', function(user) {
         console.log('User connect:'+ JSON.stringify(user));
@@ -34,17 +39,17 @@ io.on('connection', function (socket_client) {
             io.sockets.in(socket_client.room).emit('receive.user.connected', packageConnected);
         }
         
-      
         //subscribe connected user to a specific channel, 
         //later he can receive message directly from our ChatController
-        redis.subscribe(['line.message'], function (err, count) {});
-
+        redis.subscribe(['message.bot'], function (err, count) {
+            console.log(count);
+        });
+        
         // get messages send by ChatController
-        redis.on("message", function (channel, message) {
+        redis.on("message.bot", function (channel, message) {
             console.log('Receive message %s from system in channel %s', message, channel);
             socket_client.emit(channel, message);
         });
-
         
 
     });
@@ -59,6 +64,22 @@ io.on('connection', function (socket_client) {
         console.log("send package to admin ");
         console.log( packageMessages );
         io.sockets.in(socket_client.room).emit('receive.admin.message', packageMessages);
+      
+        Apps.getAppInfo(socket_client.room,function( appinfo ){
+            console.log('Appinfo');
+            console.log(appinfo);
+            if(appinfo){
+                Bot.sendTextMessage(
+                     appinfo,
+                     socket_client.user.profile.mid.split(),
+                     '1383378250',
+                     '138311608800106203',
+                     package.message,function(result){
+                        console.log(result);
+                    }
+                );
+            }
+        });
     });
     
     socket_client.on('send.admin.message', function (package) {
@@ -78,6 +99,22 @@ io.on('connection', function (socket_client) {
                     if( package.to == client.user.profile.mid ){
                         
                         client.emit('receive.user.message', packageMessages);
+                        Apps.getAppInfo(socket_client.room,function( appinfo ){
+                            console.log('Appinfo');
+                            console.log(appinfo);
+                            if(appinfo){
+                                Bot.sendTextMessage(
+                                     appinfo,
+                                     client.user.profile.mid.split(),
+                                     '1383378250',
+                                     '138311608800106203',
+                                     package.message,function(result){
+                                        console.log(result);
+                                    }
+                                );
+                            }
+                        });
+                        break;
                     }
                 }
             }
