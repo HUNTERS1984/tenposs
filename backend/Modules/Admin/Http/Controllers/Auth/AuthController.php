badb16b0228a886dbc1732c196aaedca6a84e8f3
Bang. Nguyen Khanh <bangnk@vng.com.vn>
2016-09-15 13:00:51 +0700

    check stauts = 1 when login

--- backend/Modules/Admin/Http/Controllers/Auth/AuthController.php
+++ backend/Modules/Admin/Http/Controllers/Auth/AuthController.php
@@ -113,4 +113,43 @@ class AuthController extends Controller
 		return redirect('admin/waiting');
 	}
 
+	public function login(Request $request)
+	{
+		$this->validateLogin($request);
+
+		// If the class is using the ThrottlesLogins trait, we can automatically throttle
+		// the login attempts for this application. We'll key this by the username and
+		// the IP address of the client making these requests into this application.
+		$throttles = $this->isUsingThrottlesLoginsTrait();
+
+		if ($throttles && $lockedOut = $this->hasTooManyLoginAttempts($request)) {
+			$this->fireLockoutEvent($request);
+
+			return $this->sendLockoutResponse($request);
+		}
+
+		$credentials = $this->getCredentials($request);
+
+		if (Auth::guard($this->getGuard())->attempt($credentials, $request->has('remember'))) {
+			if (Auth::user()->status == 1)
+				return $this->handleUserWasAuthenticated($request, $throttles);
+			else {
+				Auth::logout();
+				return redirect('/admin/login')
+					->withInput($request->only('username', 'remember'))
+					->withErrors([
+						'username' => "unauthorized applications",
+					]);
+			}
+		}
+
+		// If the login attempt was unsuccessful we will increment the number of attempts
+		// to login and redirect the user back to the login form. Of course, when this
+		// user surpasses their maximum number of attempts they will get locked out.
+		if ($throttles && ! $lockedOut) {
+			$this->incrementLoginAttempts($request);
+		}
+
+		return $this->sendFailedLoginResponse($request);
+	}
 }
