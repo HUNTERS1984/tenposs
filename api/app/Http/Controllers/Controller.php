bb7565ca876da8c26b72398b8ab6ff655f3206de
luong hong quan <luong.hong.quan@mqsolutions.vn>
2016-09-16 12:19:06 +0700

    fix bug

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -196,6 +196,8 @@ class Controller extends BaseController
             if (substr($relativeURL, 0, 2) === './') $relativeURL = substr($relativeURL, 2);  
             $urlInfo = parse_url($baseAbsoluteURL);  
             if ($urlInfo == false) return false;  
+            if (!isset($urlInfo['path'])) 
+                $urlInfo['path'] = '';
             $urlBasePath = substr($urlInfo['path'], 0, strrpos($urlInfo['path'],"/"));  
             $dirDepth = substr_count($urlInfo['path'], '/')-1;  
           
@@ -206,8 +208,11 @@ class Controller extends BaseController
             {  
                $urlBasePath = substr($urlBasePath, 0, strrpos($urlBasePath,"/"));  
             }  
-            $urlBase = $urlInfo['scheme'].'://'.$urlInfo['host'].$urlBasePath;  
-          
+            if (isset($urlInfo['port']))
+                $urlBase = $urlInfo['scheme'].'://'.$urlInfo['host'].':'.$urlInfo['port'].$urlBasePath;  
+            else 
+                $urlBase = $urlInfo['scheme'].'://'.$urlInfo['host'].$urlBasePath;
+
             do  
             {  
                $tempContent = $relativeURL;  
@@ -228,6 +233,6 @@ class Controller extends BaseController
                $relativeURL=$urlBase.'/'.$relativeURL;  
             }  
        }  
-       return $relativeURL;  
+       return $relativeURL; 
     }  
 }
