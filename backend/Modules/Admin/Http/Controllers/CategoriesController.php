6e254d2f89c61dfb738003a12b6a5ad5051a1982
phanvannhien <phanvannhien@gmail.com>
2016-09-19 03:31:55 +0000

    Generate appid, app secret

---
+++ backend/Modules/Admin/Http/Controllers/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Admin\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Utils\RedisControl;
+use Illuminate\Http\Request;
+use Modules\Admin\Http\Requests\ImageRequest;
+
+
+use Carbon\Carbon;
+use App\Models\News;
+use App\Models\Store;
+
+
+class CategoriesController extends Controller
+{
+    
+}
