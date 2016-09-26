2e123fe3ba2519e91eeb9e9d5958f7f8433cafb3
phanvannhien <phanvannhien@gmail.com>
2016-09-26 03:27:24 +0000

    Add 2 columns app_icon, store_image to app_stores

---
+++ backend/database/migrations/2016_09_26_032608_alter_tables_app_stores.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTablesAppStores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('app_stores', function (Blueprint $table) {
+            $table->string('app_icon',255)->nullable();
+            $table->string('store_image',255)->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
