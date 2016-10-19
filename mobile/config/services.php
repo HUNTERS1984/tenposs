bbab982adabed6ffa4866f95668a0e2a5625162f
ServeAmazon <bangnk@outlook.com>
2016-10-19 01:05:15 +0000

    add before pull

--- mobile/config/services.php
+++ mobile/config/services.php
@@ -39,19 +39,19 @@ return [
     'facebook' => [
         'client_id' => '159391921181596',
         'client_secret' => 'cc9ad24a6a123349ada67fb373067fbc',
-        'redirect' => 'https://tenposs-end-phanvannhien.c9users.io/login/callback/facebook',
+        'redirect' => 'https://'.$_SERVER['SERVER_NAME'].'/login/callback/facebook',
     ],
     
     'twitter' => [
-        'client_id' => 'RwA2MxYOwBwVC8AvjbWzL34Sw',
-        'client_secret' => 'Ise1bqJluX3ZN2SwOZw3rSPBRYOJwsQt72Pxct7S2HYRrglDKc',
-        'redirect' => 'https://tenposs-end-phanvannhien.c9users.io/login/callback/twitter',
+        'client_id' => 'qY0dnYDqh99zztg8gBWkLIFrm',
+        'client_secret' => 'Byy6PCW51zvhVrDZayLm8PhenqkHXiRIqLMpK7A5H5XNEzlKYi',
+        'redirect' => 'https://'.$_SERVER['SERVER_NAME'].'/login/callback/twitter',
     ],
     
     'instagram' => [
         'client_id' => '17d7e27257b74d05b352fc55692b2b59',
         'client_secret' => '459fecbe46f04639852268e390189d1a ',
-        'redirect' => 'https://tenposs-end-phanvannhien.c9users.io/login/callback/instagram',
+        'redirect' => 'https://'.$_SERVER['SERVER_NAME'].'/login/callback/instagram',
     ],
     
     
