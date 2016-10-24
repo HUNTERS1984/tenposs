c9894f73c95ff7b4dd1de9cc7892ade1aef08577
phanvannhien <phanvannhien@gmail.com>
2016-10-24 08:48:19 +0000

    add disconect social profile

--- mobile/resources/views/profile.blade.php
+++ mobile/resources/views/profile.blade.php
@@ -91,7 +91,7 @@
                         @if( $profile->data->user->profile->facebook_status == 1 )
                         <a href="{{ route('social.cancel',[ 'type' => 1 ]) }}" class="btn">非接続</a>
                         @else
-                        <a href="{{ $url_fb }}" class="btn">
+                        <a href="{{ $fb_url }}" class="btn">
                            連携
                         </a>
                         @endif
@@ -104,7 +104,7 @@
                         @if( $profile->data->user->profile->twitter_status == 1 )
                         <a href="{{ route('social.cancel',[ 'type' => 2 ]) }}" class="btn">非接続</a>
                         @else
-                        <a href="{{ $url_tw }}" class="btn">
+                        <a href="{{ $tw_url }}" class="btn">
                            連携
                         </a>
                         @endif
