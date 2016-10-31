45122572b0fc1b84a962e456ca19cb3972700f92
phanvannhien <phanvannhien@gmail.com>
2016-10-31 09:51:21 +0000

    update models app

--- api/app/Models/App.php
+++ api/app/Models/App.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class App extends Model
 {
     use SoftDeletes;
+    protected $table = 'apps';
     protected $dates = ['deleted_at'];
     protected $fillable = [
         'name',
