216b76c166ab42b1e6cc235fe2ecdbeb0c0183e5
phanvannhien <phanvannhien@gmail.com>
2016-11-15 13:27:30 +0700

    fix api app_info

--- api/app/Http/Controllers/AppUserController.php
+++ api/app/Http/Controllers/AppUserController.php
@@ -703,7 +703,8 @@ class AppUserController extends Controller
         }
         $rs_data = array();
         try {
-            $app_info = User::where('domain', Input::get('domain'))->with('apps')->first();
+            //$app_info = User::where('domain', Input::get('domain'))->with('apps')->first();
+            $app_info = App\Models\App::where('domain', Input::get('domain'))->first();
             if (count($app_info) > 0) {
                 if (count($app_info['apps']) > 0)
                     $rs_data = $app_info['apps'][0];
