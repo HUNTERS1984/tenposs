71deffce82a2a5a0ce6f2a10a296d0032ae07ca0
luong hong quan <luong.hong.quan@mqsolutions.vn>
2016-11-22 20:04:41 +0700

    fix news api

--- api/app/Http/Controllers/NewsController.php
+++ api/app/Http/Controllers/NewsController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Input;
 use App\Models\News;
+use App\Models\Store;
 use Mail;
 use App\Address;
 use DB;
@@ -72,6 +73,7 @@ class NewsController extends Controller
 
     public function index(Request $request)
     {
+
         $check_items = array('app_id', 'pageindex', 'pagesize', 'time', 'sig');
 
         $ret = $this->validate_param($check_items);
@@ -81,15 +83,18 @@ class NewsController extends Controller
         $check_sig_items = Config::get('api.sig_news');
         // check app_id in database
         $app = $this->_topRepository->get_app_info_array(Input::get('app_id'));
+
         if ($app == null || count($app) == 0)
             return $this->error(1004);
         //validate sig
         $ret_sig = $this->validate_sig($check_sig_items, $app['app_app_secret']);
         if ($ret_sig)
             return $ret_sig;
+
         //end validate app_id and sig
         if (Input::get('pageindex') < 1 || Input::get('pagesize') < 1)
             return $this->error(1004);
+
         $category_id = Input::get('category_id');
         $skip = (Input::get('pageindex') - 1) * Input::get('pagesize');
         //create key redis
@@ -103,17 +108,31 @@ class NewsController extends Controller
             return $this->output($this->body);
         }
         try {
-            if ($category_id > 0)
-                $total_news = News::where('new_category_id', $category_id)->whereNull('deleted_at')->count();
-            else
-                $total_news = News::whereNull('deleted_at')->count();
             $news = [];
-            if ($total_news > 0) {
-                if ($category_id > 0)
+            if ($category_id > 0) {
+                $total_news = News::where('new_category_id', $category_id)->whereNull('deleted_at')->count();
+                if ($total_news > 0) {
                     $news = News::where('new_category_id', $category_id)->whereNull('deleted_at')->skip($skip)->take(Input::get('pagesize'))->orderBy('date', 'desc')->get()->toArray();
-                else
-                    $news = News::whereNull('deleted_at')->skip($skip)->take(Input::get('pagesize'))->orderBy('date', 'desc')->get()->toArray();
-            }
+                }
+            } 
+            else {
+                $total_news = 0;
+                $stores = Store::whereAppId(Input::get('app_id'))->get();
+
+                if ($stores) {
+                    $news_cat = NewCat::whereIn('store_id', $stores->pluck('id')->toArray())->orderBy('id', 'DESC')->whereNull('deleted_at')->get();
+                   
+                    if (count($news_cat) > 0) {
+                        $total_news = News::whereIn('new_category_id',$news_cat->pluck('id')->toArray())->whereNull('deleted_at')->count();            
+                    }
+                    if ($total_news > 0)
+                    {
+                        $news = News::whereIn('new_category_id',$news_cat->pluck('id')->toArray())->whereNull('deleted_at')->skip($skip)->take(Input::get('pagesize'))->orderBy('date', 'desc')->get()->toArray();
+                    }        
+                }
+                    
+            }   
+            
             for ($i = 0; $i < count($news); $i++) {
                 $news[$i]['image_url'] = UrlHelper::convertRelativeToAbsoluteURL(Config::get('api.media_base_url'), $news[$i]['image_url']);
             }
