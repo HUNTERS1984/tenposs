4c150a6ae53ed60e5df5c1b2483f1ef9f40e83f4
phanvannhien <phanvannhien@gmail.com>
2016-11-29 21:10:45 +0700

    app icon type2, type3

---
+++ lp/offical_tenposs_backend/resources/views/admin/pages/globals/app_splash.blade.php
@@ -0,0 +1,75 @@
+<div class="modal-dialog modal-global-redesign-2" role="document">
+    <div class="modal-content">
+
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 class="modal-title" id="myModalLabel">ストア用スクリーンショット作成依頼</h4>
+        </div>
+
+        <div class="modal-body">
+            <div class="tab-header-gb">
+                <ul class="nav nav-tabs tabs-center">
+                    <li class="active">
+                        <a href="#tab-gb-re-1" aria-controls="tab-gb-re-1" role="tab" data-toggle="tab">
+                            スクリーンショット
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tab-gb-re-2" data-toggle="tab">オリジナル作成</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="tab-gb-re-1">
+                    <div class="wrapp-form-tab-gb-1">
+                        <form>
+                            <label>ストア用スクリーンショット</label>
+                            <label style="margin-bottom: 10px;display: block" class="label label-info" for="">File dementions: 750px x 1334px</label>
+                            <div id="output-upload"></div>
+                            <div class="content-tab-3-select-2">
+                                <label for="">1 枚目</label>
+                                <div class="splash-img" id="splash_image_1"></div>
+                            </div>
+                            <div class="content-tab-3-select-2">
+                                <label for="">2 枚目</label>
+                                <div class="splash-img" id="splash_image_2"></div>
+                            </div>
+                            <div class="content-tab-3-select-2">
+                                <label for="">3 枚目</label>
+                                <div class="splash-img" id="splash_image_3"></div>
+                            </div>
+                            <div class="content-tab-3-select-2">
+                                <label for="">4 枚目</label>
+                                <div class="splash-img" id="splash_image_4"></div>
+                            </div>
+                            <div class="content-tab-3-select-2">
+                                <label for="">5 枚目</label>
+                                <div class="splash-img" id="splash_image_5"></div>
+                            </div>
+                            <!--
+                             <div class="btn-tab-gb-1">
+                                 <a href="">作成依頼</a>
+                             </div>
+                                           -->
+                        </form>
+                    </div>
+                </div>
+
+                <div role="tabpanel" class="tab-pane" id="tab-gb-re-2">
+                    <div class="wrapp-form-tab-gb-2">
+                        <p>説明が入ります説明が入りまます説明</p>
+                        <p>
+                            が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入り
+                        </p>
+                        <!--
+                        <a href="">作成依頼</a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
