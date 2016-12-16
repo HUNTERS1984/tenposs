f1ab4587c440a0a8d11cb392c6d62afe20d9aa72
phanvannhien <phanvannhien@gmail.com>
2016-12-16 14:34:26 +0700

    fix profile cancel social

--- mobile/app/Http/Controllers/LoginController.php
+++ mobile/app/Http/Controllers/LoginController.php
@@ -387,13 +387,13 @@ class LoginController extends Controller
                 'social_type' => $type ,
             ));
             if( isset($curl->code) && $curl->code == 1000 ){
-                return redirect()
+                return back()
                     ->withErrors('プロファイルをキャンセルする!')
                     ->route('profile');
             }
         }
 
-        return redirect()->back()
+        return back()
             ->withErrors('プロファイルのキャンセルをキャンセルする!' )
             ->route('profile');
     }
