84ce6a5c1edd027b7536cad65d0c532fb0c06197
phanvannhien <phanvannhien@gmail.com>
2016-12-27 18:31:48 +0700

    fix approved

--- api/resources/views/admin/partials/message.blade.php
+++ api/resources/views/admin/partials/message.blade.php
@@ -1,13 +1,26 @@
 <p>&nbsp;</p>
 @if (Session::has('success'))
     <div class="alert alert-success }}">
+        @if( is_array( Session::get('success') ) ) 
+            @foreach( Session::get('success') as $key => $val )
+             {{ $val }}
+            @endforeach
+        @else
         {{ Session::get('success') }}
+        @endif
     </div>
 @endif
 
 @if (Session::has('danger'))
     <div class="alert alert-danger }}">
+         @if( is_array( Session::get('danger') ) ) 
+            @foreach( Session::get('danger') as $key => $val )
+             {{ $val }}
+            @endforeach
+        @else
         {{ Session::get('danger') }}
+        @endif
+       
     </div>
 @endif
 
